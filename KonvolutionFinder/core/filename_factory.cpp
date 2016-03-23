37f940eb9f28923ccb6eb74bdfc13c285cde77f5
Radek Vais <you@example.com>
2016-03-23 10:45:27 +0100

    obecnejsi popis ziskavani jmen FilenameFactory + metoda ziskejCestu k souboru + komentare

--- KonvolutionFinder/core/filename_factory.cpp
+++ KonvolutionFinder/core/filename_factory.cpp
@@ -1,23 +1,92 @@
 #include "filename_factory.h"
 
-#include <iostream>
-
+/**
+ * Konstruktor připraví továrnu jmen s výchozími parametry masky souborů na *.tif soubory
+ * @brief FilenameFactory::FilenameFactory
+ * @param folder_path QString cesta ke slozce
+ */
 FilenameFactory::FilenameFactory(QString folder_path) {
-    QDir image_folder(folder_path);
+    this->folder_path = folder_path;
 
-    QStringList filters;
+    filters.clear();
     filters << "*.tif";
-    image_folder.setNameFilters(filters);
 
-    name_list = image_folder.entryList();
+    initializeFactory();
+}
 
+
+/**
+ * Inicializuje továrnu jmen na začátek složky, abecedně řazeno.
+ * @brief FilenameFactory::initializeFactory
+ */
+void FilenameFactory::initializeFactory(){
+    if(folder != NULL){ // pokud již byla slozka nastavena... zruš puvodni instanci
+        delete folder;
+    }
+
+    folder = new QDir(folder_path);
+    folder->setNameFilters(filters);
+    name_list = folder->entryList();
+
+    if(name_iterator != NULL){ // pokud již byl name iterator nastaveny... zruš puvodni instanci
+        delete name_iterator;
+    }
     name_iterator = new QStringListIterator(name_list);
 }
 
+
+/**
+ * Připojí další pattern do seznamu filtrů
+ * @brief FilenameFactory::addFilePatternFilter
+ * @param filter_pattern QString file pattern
+ */
+void FilenameFactory::addFilePatternFilter(QString filter_pattern){
+    filters << filter_pattern;
+}
+
+
+/**
+ * Nastavi novou hodnotu výchozí složky. Po nastavení je třeba továrnu opět inicializovat
+ * @brief FilenameFactory::setFolderPath
+ * @param folder_path QString folder_path
+ */
+void FilenameFactory::setFolderPath(QString folder_path){
+    this->folder_path = folder_path;
+}
+
+
+/**
+ * Vrati nazev nasledujiciho souboru a posune iterator
+ * @brief FilenameFactory::getNextImageName
+ * @return nextName of NULL
+ */
 QString FilenameFactory::getNextImageName() {
     if (name_iterator->hasNext()) {
        return name_iterator->next();
     } else {
         return NULL;
     }
+}
+
+/**
+ * Vrati nazev nasledujiciho souboru a posune iterator
+ * @brief FilenameFactory::getNextImageName
+ * @return nextName of NULL
+ */
+QString FilenameFactory::getNextImagePath() {
+    if (name_iterator->hasNext()) {
+       return folder_path+""+name_iterator->next();
+    } else {
+        return NULL;
+    }
+}
+
+
+/**
+ * Factroy destructor
+ * @brief FilenameFactory::~FilenameFactory
+ */
+FilenameFactory::~FilenameFactory() {
+    delete name_iterator;
+    delete folder;
 }
--- KonvolutionFinder/core/filename_factory.h
+++ KonvolutionFinder/core/filename_factory.h
@@ -8,11 +8,30 @@
 
 class FilenameFactory
 {
+    /** prohledavana složka */
+    QString folder_path;
+    /** seznam jmen ve složce */
     QStringList name_list;
-    QStringListIterator *name_iterator;
+    /** seznam filtru nazvu souboru */
+    QStringList filters;
+
+    //dynamic objects
+    /** Qt objekt slozky */
+    QDir *folder = NULL;
+    /** iterator seznamem jmen */
+    QStringListIterator *name_iterator = NULL;
+
 public:
     FilenameFactory(QString folder_path);
+    ~FilenameFactory();
+
+    void addFilePatternFilter(QString filter_pattern);
+    void setFolderPath(QString folder_path);
+
+    void initializeFactory();
+
     QString getNextImageName();
+    QString getNextImagePath();
 };
 
 #endif // FILENAME_FACTORY_H
