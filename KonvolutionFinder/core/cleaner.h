2b6bba98d208073868e9eb06c30688d07d56379f
Radek Vais <you@example.com>
2016-03-25 11:26:07 +0100

    prvni nastrel cisteni snimku

--- KonvolutionFinder/core/cleaner.h
+++ KonvolutionFinder/core/cleaner.h
@@ -1,11 +1,18 @@
 #ifndef CLEANER_H
 #define CLEANER_H
 
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "core/filename_factory.h"
 
 class Cleaner
 {
+
+
 public:
-    Cleaner();
+    Cleaner(FilenameFactory *names);
 };
 
 #endif // CLEANER_H
