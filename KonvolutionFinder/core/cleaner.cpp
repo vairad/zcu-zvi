37527bf4b7a183c452da9af8b0c949806618ad74
Radek Vais <you@example.com>
2016-04-05 23:40:23 +0200

    predzpracovani snimku

--- KonvolutionFinder/core/cleaner.cpp
+++ KonvolutionFinder/core/cleaner.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "core/exception.h"
+#include "core/histogrammodifier.h"
 
 void Cleaner::setFactory(FilenameFactory *names){
     this->names = names;
@@ -11,33 +12,56 @@ void Cleaner::setFactory(FilenameFactory *names){
 void Cleaner::run(){
     QString name = names->getNextImageRelativePath();
     while(name != NULL){
+        counter++;
+        if(counter % 100 == 0 ){
+            std::cout << counter << std::endl;
+        }
 
-        std::cout << "Read: " << name.toStdString() <<"\n";
-        std::cout.flush();
+//        std::cout << "Read: " << name.toStdString() << std::endl;
+        cv::Mat image = cv::imread(name.toStdString(), CV_LOAD_IMAGE_GRAYSCALE);
 
-        cv::Mat image = cv::imread(name.toStdString(), CV_LOAD_IMAGE_COLOR);
-
+//         std::cout << "Image readed." << std::endl;
         if(!image.data){  // kontrola dat
             throw EmptyImageException(name.toStdString().c_str());
         }
 
- //       cv::namedWindow( "Desaty", CV_WINDOW_KEEPRATIO);// Create a window for display.
- //       cv::imshow( "Desaty", image );                   // Show our image inside it.
 
+        cv::Mat preprocessedImage = cv::Mat::zeros(image.rows, image.cols, image.type());;
+//         std::cout << "Zero Mat prepared" << std::endl;
 
-        msleep(1000);
-        cvMatToQImage(&image);
+        HistogramModifier c(255, 0);
+        c.setAlpha(1.0);
+        c.setBeta(100);
+        c.moveHistogram(&image, &preprocessedImage);
 
-       // name = names->getNextImagePath();
+//        imwrite( "../data/moved.tif", preprocessedImage);
+//        std::cout << "Histogram moved" << std::endl;
+
+        c.stretchHistogram(&preprocessedImage, &preprocessedImage, 150, 255);
+
+//        imwrite( "../data/stretched.tif", preprocessedImage);
+//        std::cout << "Histogram stretched" << std::endl;
+
+//        cv::adaptiveThreshold(preprocessedImage,preprocessedImage,255,CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY,27,2);
+        cv::threshold(preprocessedImage,preprocessedImage, 170, 255, CV_THRESH_BINARY);
+
+        cv::Mat imageToShowOld, imageToShowNew;
+        cv::cvtColor(image, imageToShowOld, CV_GRAY2BGR);
+        cv::cvtColor(preprocessedImage, imageToShowNew, CV_GRAY2BGR);
+        cvMatToQImage(&imageToShowOld, 1);
+        cvMatToQImage(&imageToShowNew, 2);
         name = names->getNextImageRelativePath();
+
+        image.release();
+        preprocessedImage.release();
     }
 }
 
 
-void Cleaner::cvMatToQImage(cv::Mat *input) {
+void Cleaner::cvMatToQImage(cv::Mat *input, int destination) {
 
     QImage *dest = new QImage((const uchar *) input->data, input->cols, input->rows, QImage::Format_RGB888);
     dest->bits(); // enforce deep copy, see documentation
 
-    emit showImage(dest); // draw prepared Image in window
+    emit showImage(dest, destination); // draw prepared Image in window
 }
--- KonvolutionFinder/core/cleaner.h
+++ KonvolutionFinder/core/cleaner.h
@@ -14,14 +14,15 @@ class Cleaner : public QThread {
     Q_OBJECT
     FilenameFactory *names;
 
+    int counter = 0;
+
     virtual void run();
 
-    void cvMatToQImage(cv::Mat *input);
+    void cvMatToQImage(cv::Mat *input, int destination);
 public:
     void setFactory(FilenameFactory *names);
-
 signals:
-    void showImage(QImage *image);
+    void showImage(QImage *image, int destination);
 };
 
 #endif // CLEANER_H
